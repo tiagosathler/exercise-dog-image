e0aa45d985a4c51bdb7d9f23d15cdfb87c8aafc8
tiagosathler <sathler@gmail.com>
2021-09-10 19:28:36 -0300

    Exercício 1: parcial

--- src/App.js
+++ src/App.js
@@ -2,29 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: '',
+      loading: true,
+    }
+
+    this.renderImage = this.renderImage.bind(this);
+    this.fecthDog = this.fecthDog.bind(this);
+  }
+  
+  async fecthDog() {
+    this.setState(
+      { loading: true},
+      async () => {
+        const requestInfo = await fetch("https://dog.ceo/api/breeds/image/random");
+        const { message } = await requestInfo.json();
+        this.setState({
+          loading: false,
+          url: message,
+        })
+      }
+    )
+  }
+  
+  componentDidMount() {
+    this.fecthDog();
+  }
+
+  renderImage() {
+    const { url } = this.state;
+    return (
+      <img src={ url } alt="Cão" />
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+    const loadingElement = <p>Loading...</p>;
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { loading ? loadingElement : this.renderImage() }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
